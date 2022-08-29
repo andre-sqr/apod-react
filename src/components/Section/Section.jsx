38d6f64a684fabbd3b2b560c62720887ed17d4de
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-08-29 20:21:59 -0300

    refactor: refatoração da requisição

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,41 +1,48 @@
-import React from 'react'
-import Button from '../Button/Button'
-import Input from '../Input/Input'
-import S from './Section.module.css'
+import React, { useEffect, useState } from "react";
+import Button from "../Button/Button";
+import Input from "../Input/Input";
+import S from "./Section.module.css";
 
 const Section = () => {
+  const [data, setData] = useState("");
 
-    const [data, setData] = useState()
+  const url = `https://api.nasa.gov/planetary/apod?api_key=cfc6pA7kyAgQdATFivgeENC3WLslXp04aHxCbCBC&date=${data}`;
 
-    const handleChange = (e, data) => {
-        setData(e.target.value)
-    }
+  const handleReq = async () => {
+    let response = await fetch(url);
+    const json = await response.json();
+    // setData([...data,...json.copyright]);
+    console.log(json.copyright);
+  };
 
-    const handleClick = ()=> {
+  const handleChange = (e, data) => {
+    setData(e.target.value);
+  };
 
-    }
+  const handleClick = (e) => {
+    e.preventDefault();
+    handleReq();
+    console.log(data);
+  };
 
-    const url = `https://api.nasa.gov/planetary/apod?api_key=cfc6pA7kyAgQdATFivgeENC3WLslXp04aHxCbCBC&date=${data}`
-
-
-    const handleReq = async ()=> {
-        let response = await fetch(url)
-        .then(response => {
-            // lida com a resposta
-        })
-        .catch(response => {
-            // lida com o erro
-        })
-    } 
+  useEffect(() => {
+    handleReq();
+  }, [data]);
 
   return (
     <section>
-        <div>
-        <Input type='date' style={S.input} onChange={(e)=>{handleChange(e, 'data')}}/>
-        <Button style={S.button} onClick={handleClick} text={'Enviar'}/> 
-        </div>
+      <div>
+        <Input
+          type="date"
+          style={S.input}
+          onChange={(e) => {
+            handleChange(e, "data");
+          }}
+        />
+        <Button style={S.button} onClick={handleClick} text={"Enviar"} />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Section
+export default Section;
